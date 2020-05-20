9f848e043ca25b97f0cb87d5ffa87ca83c223710
takuya hakiri <tyapi4@yahoo.co.jp>
2020-05-20 22:08:58 +0900

    add : 幹事の自動設定機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :members
-  resources :groups
-  resources :join_groups
+  resources :groups do
+    patch 'set_organizer', on: :member
+  end
+  resources :join_groups, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
